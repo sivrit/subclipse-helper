f7abcf6d3746616ee45d699c45d2cebe43f28379
Tristan Bartement <github@tristan-bartement.fr>
2011-08-07 11:30:34 +0200

    With this the scala version of createWorkingSets works

--- fr.sivrit.svn.helper.scala/src/fr/sivrit/svn/helper/scala/SvnHelperScala.scala
+++ fr.sivrit.svn.helper.scala/src/fr/sivrit/svn/helper/scala/SvnHelperScala.scala
@@ -1,16 +1,30 @@
 package fr.sivrit.svn.helper.scala
 
-import fr.sivrit.svn.helper.{ Preferences, ISvnHelper }
-import fr.sivrit.svn.helper.scala.core.ProjectDeps
-import fr.sivrit.svn.helper.scala.svn.{ SvnSwitch, SvnCheckOut, SvnCrawler }
-import org.eclipse.core.resources.{ ResourcesPlugin, IProject }
+import scala.util.matching.Regex
+
+import org.eclipse.core.resources.IProject
+import org.eclipse.core.resources.ResourcesPlugin
+import org.eclipse.core.runtime.IAdaptable
+import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart
+import org.eclipse.jdt.ui.JavaUI
 import org.eclipse.jface.dialogs.MessageDialog
 import org.eclipse.team.core.RepositoryProvider
-import org.eclipse.ui.{ IWorkingSet, IWorkingSetManager }
 import org.eclipse.ui.internal.WorkbenchPlugin
-import org.tigris.subversion.subclipse.core.{ SVNProviderPlugin, ISVNRepositoryLocation }
+import org.eclipse.ui.IWorkbench
+import org.eclipse.ui.IWorkingSet
+import org.eclipse.ui.IWorkingSetManager
+import org.eclipse.ui.PlatformUI
+import org.eclipse.ui.WorkbenchException
+import org.tigris.subversion.subclipse.core.ISVNRepositoryLocation
+import org.tigris.subversion.subclipse.core.SVNProviderPlugin
 import org.tigris.subversion.svnclientadapter.SVNUrl
-import scala.util.matching.Regex
+
+import fr.sivrit.svn.helper.scala.core.ProjectDeps
+import fr.sivrit.svn.helper.scala.svn.SvnCheckOut
+import fr.sivrit.svn.helper.scala.svn.SvnCrawler
+import fr.sivrit.svn.helper.scala.svn.SvnSwitch
+import fr.sivrit.svn.helper.ISvnHelper
+import fr.sivrit.svn.helper.Preferences
 
 class SvnHelperScala extends ISvnHelper {
 
@@ -38,15 +52,51 @@ class SvnHelperScala extends ISvnHelper {
     true
   }
 
-  def resolveDependencies(svnUrl: Array[SVNUrl]): Boolean = {
+  def resolveDependencies(svnUrls: Array[SVNUrl]): Boolean = {
     false
   }
 
   def createWorkingSets(urls: Array[SVNUrl]): Boolean = {
+    // Find the projects to dispatch
     val newWS: Map[String, List[IProject]] = SvnHelperScala.resolveWorkingSetFromSvn(urls)
-    val workingSets: Map[String, IWorkingSet] = SvnHelperScala.createWorkingSets(newWS.keys);
 
-    val wsManager: IWorkingSetManager = WorkbenchPlugin.getDefault().getWorkingSetManager();
+    // Confirm the operation
+    val msg: StringBuilder = new StringBuilder("The following WorkingSets will be created:\n")
+    for (entry <- newWS) {
+      msg.append(entry._1).append(" - ").append(entry._2.length).append(" projects\n")
+    }
+
+    if (!MessageDialog.openQuestion(null, "Define Working Sets", msg.toString())) {
+      return false;
+    }
+
+    // Show the Java perspective, the package explorer and witch to a view
+    // by working sets.
+    val workbench: IWorkbench = PlatformUI.getWorkbench()
+    try {
+      workbench.showPerspective(JavaUI.ID_PERSPECTIVE, workbench.getActiveWorkbenchWindow());
+    } catch {
+      case e: WorkbenchException =>
+        e.printStackTrace();
+        return false;
+    }
+
+    val explo: PackageExplorerPart = PackageExplorerPart.openInActivePerspective()
+    if (explo.getRootMode() == PackageExplorerPart.PROJECTS_AS_ROOTS) {
+      explo.rootModeChanged(PackageExplorerPart.WORKING_SETS_AS_ROOTS)
+    }
+
+    // Create missing IWorkingSet and index all WS by name
+    val workingSets: Map[String, IWorkingSet] = SvnHelperScala.createWorkingSets(newWS.keys)
+
+    // Remove the projects we will dispatch from existing WS
+    var allProjects: Set[IProject] = Set()
+    for (val projects <- newWS.values) {
+      allProjects ++= projects;
+    }
+    SvnHelperScala.removeProjectsFromWorkingSets(workingSets.values, allProjects);
+
+    val wsManager: IWorkingSetManager = WorkbenchPlugin.getDefault().getWorkingSetManager()
     for ((wsName, projects) <- newWS) {
       val workingSet = Array(workingSets.getOrElse(wsName, null))
 
@@ -104,8 +154,9 @@ object SvnHelperScala {
     for (wsName <- wsNames) {
       if (!workingSets.contains(wsName)) {
         val ws = wsManager.createWorkingSet(wsName, Array.empty)
+        ws.setId("org.eclipse.jdt.ui.JavaWorkingSetPage")
         wsManager.addWorkingSet(ws)
-        workingSets += ((wsName, ws))
+        workingSets += wsName -> ws
       }
     }
 
@@ -145,5 +196,36 @@ object SvnHelperScala {
     }
 
     newWS
+  }
+
+  private def removeProjectsFromWorkingSets(sets: Iterable[IWorkingSet],
+    projectsToRemove: Set[IProject]) = {
+    for (ws <- sets) {
+      removeProjectsFromWorkingSet(ws, projectsToRemove);
+    }
+  }
+
+  private def removeProjectsFromWorkingSet(ws: IWorkingSet,
+    projectsToRemove: Set[IProject]) = {
+    val content = ws.getElements();
+
+    var lastElement = content.length - 1;
+    var idx = 0;
+    while (idx <= lastElement) {
+      val item: IAdaptable = content(idx)
+      item.getAdapter(classOf[IProject]) match {
+        case project: IProject if projectsToRemove.contains(project) => {
+          content(idx) = content(lastElement);
+          content(lastElement) = null;
+          lastElement -= 1;
+        }
+        case _ => idx += 1;
+      }
+    }
+
+    val newContent = new Array[IAdaptable](lastElement + 1);
+    System.arraycopy(content, 0, newContent, 0, lastElement + 1);
+
+    ws.setElements(newContent);
   }
 }
